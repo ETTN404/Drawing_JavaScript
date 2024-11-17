f0c247170979fb3c761bc36fd0bbbb2fe676f3d6
ETTN404 <et8302tn@gmail.com>
2024-11-17 04:45:53 +0300

    version 1
    this code is simple drawing webtool made with html css and mostly JavaScript and this is the initial commit

---
+++ python.py
@@ -0,0 +1,29 @@
+print('hello world')
+
+a=5
+b=2
+
+# print(a+b)
+# print(a/b)
+# print(a-b)
+# print(a%b)
+# print(a*b)
+# print(a<b)
+# print(a>b)
+# print(a!=b)
+
+
+# print(a and b)
+
+# print(a or b)
+
+
+string ="im form the department of computer science studing python lab session"
+
+print(string[0])
+print(string[3:7])
+
+print(len(string))
+
+
+
